4bee26d719febacd88b7b9c88d83ac47db6ba2b4
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2022-04-13 21:06:32 +0200

    main.py - added division into 3 sets

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import keras as ks
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import sklearn as skl
 
 # ---------Global variables-------------
 FEATURE_COUNT = 4096 + 512
@@ -27,9 +28,16 @@ def import_dataset():
 # preprocessing function
 # takes array of full dataset as input
 # returns array with preprocessing completed
-def preprocessing(x):
-    return x
+def preprocessing(x, y):
+    x, y = skl.utils.shuffle(x, y)
 
+    def divide(arr):
+        length = arr.shape[0]
+        return [arr[:length // 3],
+                arr[length // 3:2 * length // 3],
+                arr[2 * length // 3:]]
+
+    return divide(x), divide(y)
 
 # build_mlm function
 # returns constructed mlm
@@ -38,7 +46,7 @@ def build_mlm():
     layers = [tf.keras.Input(shape=(FEATURE_COUNT,)),
               tf.keras.layers.Dense(100, activation="relu"),
               tf.keras.layers.Dense(100, activation="relu"),
-              tf.keras.layers.Dense(1, activation="heaviside")
+              tf.keras.layers.Dense(1, activation="sigmoid")
               ]
     model = tf.keras.Sequential(layers)
     model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
@@ -56,15 +64,23 @@ def train(model, x, y):
 # output_results function
 # outputs predictions to csv, generates graphics and console output
 def output_results(model, x, y):
-    print("Results: ", x.shape, y.shape,
-          x[0, 0], x[0, -1], x[-1, 0], x[-1, -1],
-          y[0, 0], y[0, 1], y[-1, 0], y[-1, 1])
+    print("Results: ", model.evaluate(x, y))
 
 
 # run the program if this is the main script
 if __name__ == '__main__':
-    train_x, train_y = import_dataset()
-    train_x = preprocessing(train_x)
-    mlm = build_mlm()
-    mlm = train(mlm, train_x, train_y)
-    output_results(mlm, train_x, train_y)
+    full_x, full_y = import_dataset()
+    x_sets, y_sets = preprocessing(full_x, full_y)
+
+    # build 3 mlms, train each on 2/3 sets, save third for validation
+    set_count = 3
+    for i in range(set_count):
+        mlm = build_mlm()
+
+        x_train = np.concatenate((x_sets[i], x_sets[(i+1) % set_count]))
+        y_train = np.concatenate((y_sets[i], y_sets[(i+1) % set_count]))
+        x_valid = x_sets[(i+2) % set_count]
+        y_valid = x_sets[(i+2) % set_count]
+
+        mlm = train(mlm, x_train, y_train)
+        output_results(mlm, x_valid, y_valid)
