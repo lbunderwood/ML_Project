319b28a79611daa97a94e16c147783f462300c90
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2022-05-17 15:27:47 +0100

    knn imputing added

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ FEATURE_COUNT = 4096 + 512
 
 # import_dataset function
 # returns arrays containing test data and predictions
-def import_dataset():
+def import_dataset(dataset):
     print("\nStarted data import...")
-    data = pd.read_csv("training1.csv")
+    data = pd.read_csv(dataset)
     x = data.iloc[:, :FEATURE_COUNT]
     y = data.iloc[:, FEATURE_COUNT:]
     print("Data import complete!\n")
@@ -31,14 +31,67 @@ def import_dataset():
 # preprocessing function
 # takes array of full dataset as input
 # returns array with preprocessing completed
-def preprocessing(x, y, cnn_features=4096, gist_features=512):
+def preprocessing(x, y, x2, y2, k_neighbors=5, cnn_features=4096, gist_features=512):
     print("\nStarted preprocessing...")
     # throw out confidence labels for now
     y = y[:, 0]
+    y2 = y2[:, 0]
+
+    num = 0
+    # do KNN imputing on x2
+    for row2 in x2:
+        num += 1
+        # array of distances between row2 and all rows in x
+        distances = np.zeros(x.shape[0])
+        # array of the values from row2 that are not nan
+        row2_nums = np.zeros(FEATURE_COUNT)
+        # array of indices corresponding to the values in row2_nums
+        row2_idx = np.zeros_like(row2_nums)
+        # counter for non-nan elements of row2
+        num_count = 0
+        for i in range(len(row2)):
+            if not pd.isna(row2[i]):
+                row2_nums[num_count] = row2[i]
+                row2_idx[num_count] = i
+        row2_nums = row2_nums[:num_count]
+        row2_idx = row2_idx[:num_count]
+
+        for i in range(x.shape[0]):
+            # array of corresponding values to row2_nums from row
+            row_nums = np.zeros_like(row2_nums)
+            for j in range(len(row2_idx)):
+                row_nums[j] = x[i][int(row2_idx[j])]
+            # compute euclidean distance, store in distances
+            dist = np.linalg.norm(row2_nums-row_nums)
+            distances[i] = dist
+
+            # check to make sure no nans make it through
+            if pd.isna(dist):
+                raise ValueError("NaN found in calculated distance during KNN")
+
+        # get indices that would sort distances
+        distIdx = np.argsort(distances)
+        # use sorted indices to get the indices of the k nearest neighbors
+        k_nearest_idx = distIdx[:k_neighbors]
+
+        # for all nan values, calculate the average value from k nearest
+        for i in range(len(row2)):
+            if pd.isna(row2[i]):
+                # get k nearest
+                k_values = np.zeros_like(k_nearest_idx)
+                for j in range(k_neighbors):
+                    k_values[j] = x[k_nearest_idx[j], i]
+
+                # average them and assign new value
+                avg = np.average(k_values)
+                row2[i] = avg
+
+    # combine x, x2 and y, y2
+    x = np.append(x, x2, axis=0)
+    y = np.append(y, y2)
 
     # normalize features to mean 0, sd 1
-    features = x.shape[1]
-    for i in range(features):
+    for i in range(FEATURE_COUNT):
         feature = x[:, i]
         x[:, i] = (feature - np.mean(feature)) / np.std(feature)
 
@@ -56,7 +109,6 @@ def preprocessing(x, y, cnn_features=4096, gist_features=512):
     x_gist = pca_gist.transform(x_gist)
 
     x = np.append(x_cnn, x_gist, axis=1)
-    print("x size = ", x.shape)
 
     # shuffle data
     x, y = skl.utils.shuffle(x, y)
@@ -110,8 +162,10 @@ def output_results(model, x, y):
 
 # run the program if this is the main script
 if __name__ == '__main__':
-    full_x, full_y = import_dataset()
-    x_sets, y_sets = preprocessing(full_x, full_y, cnn_features=4, gist_features=4)
+    full_x, full_y = import_dataset("training1.csv")
+    partial_x, partial_y = import_dataset("training2.csv")
+
+    x_sets, y_sets = preprocessing(full_x, full_y, partial_x, partial_y, cnn_features=4, gist_features=4)
     # build 3 mlms, train each on 2/3 sets, save third for testing
     set_count = 3
     for k in range(set_count):
